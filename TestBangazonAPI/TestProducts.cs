7cc15f3ca4d999f9f26aad6c9c2a32de1b53d2e4
MeagMueller <46437198+MeagMueller@users.noreply.github.com>
2019-06-26 11:40:19 -0500

    Working on Product testing

--- TestBangazonAPI/TestProducts.cs
+++ TestBangazonAPI/TestProducts.cs
@@ -1,10 +1,31 @@
-﻿using System;
+﻿using BangazonAPI.Models;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Net;
 using System.Text;
+using System.Threading.Tasks;
+using Xunit;
 
 namespace TestBangazonAPI
 {
-    class TestProducts
+    public class TestProducts
     {
+        /*This Tests that you can get all Products*/
+        [Fact]
+        public async Task Test_Get_All_Products()
+        {
+
+            using (var client = new APIClientProvider().Client)
+            {
+                var response = await client.GetAsync("/api/PaymentTypes");
+
+                string responseBody = await response.Content.ReadAsStringAsync();
+                var paymentTypesList = JsonConvert.DeserializeObject<List<Products>>(responseBody);
+
+                Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+                Assert.True(paymentTypesList.Count > 0);
+            }
+        }
     }
 }

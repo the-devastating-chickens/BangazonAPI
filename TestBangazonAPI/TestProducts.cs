1dc2d9aeb59a589015db0ac7565f4e5447158d24
MeagMueller <46437198+MeagMueller@users.noreply.github.com>
2019-06-26 15:47:02 -0500

    Working

--- TestBangazonAPI/TestProducts.cs
+++ TestBangazonAPI/TestProducts.cs
@@ -27,5 +27,29 @@ namespace TestBangazonAPI
                 Assert.True(paymentTypesList.Count > 0);
             }
         }
+
+        /*This Tests that you can get one single Product*/
+        [Fact]
+        public async Task Test_Get_Single_Products()
+        {
+
+            using (var client = new APIClientProvider().Client)
+            {
+                var response = await client.GetAsync("/api/Products/1");
+
+
+                string responseBody = await response.Content.ReadAsStringAsync();
+                var product = JsonConvert.DeserializeObject<Products>(responseBody);
+
+                Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+                Assert.Equal(1, product.ProductTypeId);
+                Assert.Equal(1, product.CustomerId);
+                Assert.Equal(10.0000, product.Price);
+                Assert.Equal("Lord of the Rings", product.Title);
+                Assert.Equal("A book about a ring and an epic journey to a volcano.", product.Description);
+                Assert.Equal(5, product.Quantity);
+                Assert.NotNull(product);
+            }
+        }
     }
 }

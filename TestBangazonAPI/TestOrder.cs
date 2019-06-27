fd39ade80e8ef168b0811670309897c66a33c555
jonathan schaffer <jonizzyschaffer@gmail.com>
2019-06-27 15:33:47 -0500

    added one test

---
+++ TestBangazonAPI/TestOrder.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using Newtonsoft.Json;
+using Xunit;
+using BangazonAPI.Models;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace TestBangazonAPI
+{
+    public class TestOrder
+    {
+        [Fact]
+        public async Task Test_Get_All_Orders()
+        {
+            using (var client = new APIClientProvider().Client)
+            {
+                /*
+                    ARRANGE
+                */
+
+
+                /*
+                    ACT
+                */
+                var response = await client.GetAsync("/api/Order");
+
+
+                string responseBody = await response.Content.ReadAsStringAsync();
+                var orders = JsonConvert.DeserializeObject<List<Order>>(responseBody);
+
+                /*
+                    ASSERT
+                */
+                Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+                Assert.True(orders.Count > 0);
+            }
+        }
+    }
+}

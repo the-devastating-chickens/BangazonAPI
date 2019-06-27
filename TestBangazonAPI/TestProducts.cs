7f9385a76dc4a23edb87d0cf5d9a488bf5d1720b
MeagMueller <46437198+MeagMueller@users.noreply.github.com>
2019-06-27 11:18:32 -0500

    Testing for Get Single Product and Create and Delete completed

--- TestBangazonAPI/TestProducts.cs
+++ TestBangazonAPI/TestProducts.cs
@@ -3,6 +3,7 @@ using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Xunit;
@@ -44,11 +45,51 @@ namespace TestBangazonAPI
                 Assert.Equal(HttpStatusCode.OK, response.StatusCode);
                 Assert.Equal(1, product.ProductTypeId);
                 Assert.Equal(1, product.CustomerId);
-                Assert.Equal(10.0000, product.Price);
+                Assert.Equal((decimal)10.0000, product.Price);
                 Assert.Equal("Lord of the Rings", product.Title);
                 Assert.Equal("A book about a ring and an epic journey to a volcano.", product.Description);
                 Assert.Equal(5, product.Quantity);
                 Assert.NotNull(product);
+            }
+        }
+
+        /*This tests that you can create and then delete one single Products*/
+        [Fact]
+        public async Task Test_Create_And_Delete_Products()
+        {
+            using (var client = new APIClientProvider().Client)
+            {
+                Products BusterSword = new Products
+                {
+                    ProductTypeId = 4,
+                    CustomerId = 2,
+                    Price = (decimal)1000.0000,
+                    Title = "Buster Sword",
+                    Description = "A big ass sword.",
+                    Quantity = 1
+                };
+                var BusterSwordAsJSON = JsonConvert.SerializeObject(BusterSword);
+
+
+                var response = await client.PostAsync(
+                    "/api/Products",
+                    new StringContent(BusterSwordAsJSON, Encoding.UTF8, "application/json")
+                );
+
+
+                string responseBody = await response.Content.ReadAsStringAsync();
+                var newBusterSword = JsonConvert.DeserializeObject<Products>(responseBody);
+
+                Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+                Assert.Equal(4, newBusterSword.ProductTypeId);
+                Assert.Equal(2, newBusterSword.CustomerId);
+                Assert.Equal((decimal)1000.0000, newBusterSword.Price);
+                Assert.Equal("A big ass sword.", newBusterSword.Description);
+                Assert.Equal(1, newBusterSword.Quantity);
+
+
+                var deleteResponse = await client.DeleteAsync($"/api/Products/{newBusterSword.Id}");
+                Assert.Equal(HttpStatusCode.NoContent, deleteResponse.StatusCode);
             }
         }
     }
